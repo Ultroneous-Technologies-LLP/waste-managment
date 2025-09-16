c4986cdea37a586efec6dfd37d4928991fd11f7f
Vijaydev19 <dev19.ultroneous@gmail.com>
2025-09-16 13:56:42 +0530

    feat: created transforming waste into resources section

--- src/components/home/index.ts
+++ src/components/home/index.ts
@@ -2,5 +2,12 @@ import HomeHero from "./HomeHero";
 import AboutUs from "./AboutUs";
 import WasteManagementServices from "./WasteManagementServices";
 import IndustriesWeServe from "./IndustriesWeServe";
+import TransformingWaste from "./TransformingWaste";
 
-export { HomeHero, AboutUs, WasteManagementServices, IndustriesWeServe };
+export {
+  HomeHero,
+  AboutUs,
+  WasteManagementServices,
+  IndustriesWeServe,
+  TransformingWaste,
+};
