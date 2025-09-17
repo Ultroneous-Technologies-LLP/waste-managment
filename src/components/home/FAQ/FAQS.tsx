77b6cc44ea8fd537a14c81d665c95c666d952c96
Vijaydev19 <dev19.ultroneous@gmail.com>
2025-09-17 16:25:01 +0530

    fix: bugs

--- src/components/home/FAQ/FAQS.tsx
+++ src/components/home/FAQ/FAQS.tsx
@@ -15,7 +15,8 @@ const FAQS: FC<FAQSProps> = ({ data }) => {
       {data.map((faq) => (
         <div
           key={faq.id}
-          className="bg-white rounded-3xl md:rounded-4xl p-4 xl:p-6"
+          className="bg-white rounded-3xl md:rounded-4xl p-4 xl:p-6 cursor-pointer"
+          onClick={() => setOpenId(openId === faq.id ? null : faq.id)}
         >
           <div className="flex justify-between gap-4">
             <h3 className="text-base xl:text-2xl/7.5 max-w-63 md:max-w-154 xl:max-w-304.5 w-full">
@@ -28,7 +29,6 @@ const FAQS: FC<FAQSProps> = ({ data }) => {
               svgClassName={`${
                 openId === faq.id ? "rotate-90" : "-rotate-90"
               } text-black`}
-              onClick={() => setOpenId(openId === faq.id ? null : faq.id)}
             />
           </div>
           {openId === faq.id && <p className="text-base pt-3">{faq.ans}</p>}
