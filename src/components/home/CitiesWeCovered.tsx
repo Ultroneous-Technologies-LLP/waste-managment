1004ac344a75baff6c035980afef3d97f628a250
Vijaydev19 <dev19.ultroneous@gmail.com>
2025-09-17 16:25:01 +0530

    fix: bugs

--- src/components/home/CitiesWeCovered.tsx
+++ src/components/home/CitiesWeCovered.tsx
@@ -19,14 +19,16 @@ const CitiesWeCovered: FC<CitiesWeCoveredProps> = ({ data }) => {
             key={value.id}
             className="relative group py-6 px-13 md:py-4 md:px-8 xl:py-6 xl:px-14 bg-anti-flash-white text-base md:text-xl xl:text-2xl font-normal text-black rounded-full overflow-hidden"
           >
-            <span className="relative z-10">{value.cityName}</span>
-
+            <span className="relative z-20 group-hover:text-white text-black">
+              {value.cityName}
+            </span>
             <Image
               alt={value.alt}
               src={value.src}
               fill
               className="absolute inset-0 w-full h-full object-cover opacity-0 group-hover:opacity-100 transition-opacity duration-300"
             />
+            <div className="absolute inset-0 bg-black/40 opacity-0 group-hover:opacity-100 transition-opacity duration-300 z-10" />
           </div>
         ))}
       </div>
