70987cf1557212f4094f5f5694b3ffc8564ada7e
Vijaydev19 <dev19.ultroneous@gmail.com>
2025-11-10 10:23:59 +0530

    feat: added eslint ruls

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   reactStrictMode: true,
-  experimental: {
-    optimizeCss: true, // enables critters
-  },
 };
 
 export default nextConfig;
